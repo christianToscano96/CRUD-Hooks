1be5e17681b719e5b654cf29808db632e7167d1a
Christian Toscano <chrismarcadopago@gmail.com>
2019-08-05 20:40:32 -0300

    enlaces a las rutas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Products from './component/Products';
 import AddProduct from './component/AddProduct';
 import EditProduct from './component/EditProduct';
+import Product from './component/Product';
 
 
 function App() {
@@ -13,7 +14,10 @@ function App() {
     <div className="App">
          <Router>
             <Switch>
-
+                <Route exact path="/new-product/new" component={AddProduct} />
+                <Route exact path="/new-product" component={AddProduct} />
+                <Route exact path="/products" component={Products} />
+                <Route exact path="/products/edit/:id" component={EditProduct} />
             </Switch>
          </Router>
     </div>
---
+++ src/component/Product.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Product(){
+    return(
+        <h1>Desde Producto</h1>
+    )
+}
+
+export default Product;
