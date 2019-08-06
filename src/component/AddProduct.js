387e76d92c59e017c094e4a8e564237b2659aab0
Christian Toscano <chrismarcadopago@gmail.com>
2019-08-06 20:00:53 -0300

    creacion de formulario en componente gregar producto

--- src/component/AddProduct.js
+++ src/component/AddProduct.js
@@ -2,7 +2,83 @@ import React from 'react';
 
 function AddProduct() {
     return(
-        <h1 className="text-center">Agregar Nuevo Producto</h1>
+        <div classsName="col-md-8 mx-auto">
+            <h1 className="text-center">Agregar Nuevo Producto</h1>
+            <hr></hr>
+
+            <form 
+                className="mt-5 p-2 shadow rounded"
+            >
+                <div className="form-group p-2 pt-2 ">
+                    <label>Nombre del Platillo</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        placeholder="Nombre Platillo"
+                    />
+                </div>
+                <div className="form-group  pt-2">
+                    <label>Nombre del Platillo</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        name="price"
+                        placeholder="Precio del Platillo"
+                    />
+                </div>
+
+                <legen className="text-center p-2 pt-2">Categoría</legen>
+                <div className="text-center py-2 shadow  pt-2 rounded">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="postre"
+                        />
+                        <lebel className="form-check-label">
+                            Postre
+                        </lebel>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="bebida"
+                        />
+                        <lebel className="form-check-label">
+                            Bebida
+                        </lebel>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="cortes"
+                        />
+                        <lebel className="form-check-label">
+                            Cortes
+                        </lebel>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="category"
+                            value="ensalada"
+                        />
+                        <lebel className="form-check-label">
+                            Ensalada
+                        </lebel>
+                    </div>
+                </div>
+
+                <input type="submit" className="mb-3 col-md-2 font-weight-bold text-uppercase mt-5 btn btn-success btn-block py-3" value="Agregar Producto" />
+            </form>
+        </div> 
     )
 }
 
--- src/component/Products.js
+++ src/component/Products.js
@@ -5,6 +5,7 @@ function Products({products}) {
     return(
         <Fragment>
           <h1 className="text-center">Productos</h1>
+          <hr></hr>
 
           <ul className="list-group mt-5">
               {products.map(product => (
